cc9a270ef7a1319e1b65196789a018bd68f580f8
curiositly <curiositlyxd@gmail.com>
2022-10-01 15:54:41 -0800

    Switch discordwebhook library to official discord.py library

--- in_stock.py
+++ in_stock.py
@@ -1,38 +1,41 @@
 import time
 
+import discord
 import requests
-from bs4 import BeautifulSoup
 import toml
-from discord_webhook import DiscordEmbed, DiscordWebhook
+from bs4 import BeautifulSoup
 
 config = toml.load("config.toml")
 
-webhook = DiscordWebhook(
-                        url=config['webhook']
-    )
+webhook = discord.SyncWebhook.from_url(config["webhook"])
+
 
 def notify():
 
-    embed = DiscordEmbed(
-                            title=f'🎉 ITEM INSTOCK', description='', color='7CFC00')
+    embed = discord.Embed(
+        title=f"🎉 ITEM INSTOCK", color=0xCFC00, timestamp=discord.utils.utcnow()
+    )
 
-    embed.set_author(name=f"Polyphia", url='https://www.polyphia.com/',
-                    icon_url='https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615')  # set author
+    embed.set_author(
+        name=f"Polyphia",
+        url="https://www.polyphia.com/",
+        icon_url="https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615",
+    )  # set author
 
-    embed.set_image(url='')  # set image
+    embed.set_image(url="")  # set image
 
-    embed.set_thumbnail(url='https://cdn.shopify.com/s/files/1/0271/6018/2883/products/WHITE_SILVERSMUSH_1024x1024@2x.png?v=1662044356')  # set thumbnail
+    embed.set_thumbnail(
+        url="https://cdn.shopify.com/s/files/1/0271/6018/2883/products/WHITE_SILVERSMUSH_1024x1024@2x.png?v=1662044356"
+    )  # set thumbnail
 
-    embed.set_footer(text='nic#0002',
-                    icon_url='https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png')  # set footer
+    embed.set_footer(
+        text="nic#0002",
+        icon_url="https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png",
+    )  # set footer
 
-    embed.set_timestamp()  # set timestamp (default is now)
+    embed.add_field(name="URL", value=f"{config['url']}s", inline=False)
 
-    embed.add_embed_field(name='URL', value=f"{config['url']}s", inline=False)
-
-    webhook.add_embed(embed)
-    webhook.set_content(f"<@{config['my_id']}>")
-    webhook.execute()
+    webhook.send(content=f"<@{config['my_id']}>", embed=embed)
 
 
 def check(url, headers):
@@ -43,31 +46,34 @@ def check(url, headers):
         timeout=5,
     )
 
-
     data.raise_for_status()
 
-
     if data.status_code != 200:
-        embed = DiscordEmbed(
-                            title=f'❌ ERROR {data.status_code}', description=f'@{config["my_id"]}', color='EE4B2B')
+        embed = discord.Embed(
+            title=f"❌ ERROR {data.status_code}",
+            description=f'@{config["my_id"]}',
+            color=0xEE4B2B,
+            timestamp=discord.utils.utcnow(),
+        )
 
-        embed.set_author(name=f"Polyphia", url='https://www.polyphia.com/',
-                        icon_url='https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615')  # set author
+        embed.set_author(
+            name=f"Polyphia",
+            url="https://www.polyphia.com/",
+            icon_url="https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615",
+        )  # set author
 
-        embed.set_image(url='')  # set image
+        embed.set_image(url="")  # set image
 
-        embed.set_thumbnail(url='')  # set thumbnail
+        embed.set_thumbnail(url="")  # set thumbnail
 
-        embed.set_footer(text='nic#0002',
-                        icon_url='https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png')  # set footer
+        embed.set_footer(
+            text="nic#0002",
+            icon_url="https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png",
+        )  # set footer
 
-        embed.set_timestamp()  # set timestamp (default is now)
+        embed.add_field(name="URL", value=f"{config['url']}s", inline=False)
 
-        embed.add_embed_field(name='URL', value=f"{config['url']}s", inline=False)
-
-        webhook.add_embed(embed)
-        webhook.set_content(f"<@{config['my_id']}>")
-        webhook.execute()
+        webhook.send(content=f"<@{config['my_id']}>", embed=embed)
 
         print(data.status_code)
         exit()
@@ -77,53 +83,51 @@ def check(url, headers):
         x = BeautifulSoup(data.content, "html.parser")
 
         status = x.find(attrs={"aria-label": "Sold out"})
-            
+
         if status is None:
             notify()
             time.ctime()
-            print(
-                f"[{time.ctime()}] -- INSTOCK"
-            )
+            print(f"[{time.ctime()}] -- INSTOCK")
             return
         else:
             time.ctime()
-            print(
-                f"[{time.ctime()}] -- OOS"
-            )
+            print(f"[{time.ctime()}] -- OOS")
 
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
 }
 
-print(
-    f"SCANNING {config['url']}"
+print(f"SCANNING {config['url']}")
+
+embed = discord.Embed(
+    title=f"🔎 STARTED SCANNING", color=0x161955, timestamp=discord.utils.utcnow()
 )
 
-embed = DiscordEmbed(
-                        title=f'🔎 STARTED SCANNING', description='', color='161955')
+embed.set_author(
+    name=f"Polyphia",
+    url="https://www.polyphia.com/",
+    icon_url="https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615",
+)  # set author
 
-embed.set_author(name=f"Polyphia", url='https://www.polyphia.com/',
-                icon_url='https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615')  # set author
+embed.set_image(url="")  # set image
 
-embed.set_image(url='')  # set image
+embed.set_thumbnail(
+    url="https://cdn.shopify.com/s/files/1/0271/6018/2883/products/WHITE_SILVERSMUSH_1024x1024@2x.png?v=1662044356"
+)  # set thumbnail
 
-embed.set_thumbnail(url='https://cdn.shopify.com/s/files/1/0271/6018/2883/products/WHITE_SILVERSMUSH_1024x1024@2x.png?v=1662044356')  # set thumbnail
+embed.set_footer(
+    text="nic#0002",
+    icon_url="https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png",
+)  # set footer
 
-embed.set_footer(text='nic#0002',
-                icon_url='https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png')  # set footer
+embed.add_field(name="URL", value=f"{config['url']}s", inline=False)
 
-embed.set_timestamp()  # set timestamp (default is now)
-
-embed.add_embed_field(name='URL', value=f"{config['url']}s", inline=False)
-
-webhook.add_embed(embed)
-webhook.set_content(f"<@{config['my_id']}>")
-webhook.execute()
+webhook.send(content=f"<@{config['my_id']}>", embed=embed)
 
 while True:
     check(
-        url=config['url'], 
+        url=config["url"],
         headers=headers,
-        )
+    )
     time.sleep(60)
