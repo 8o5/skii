8be26df7232b7153dd561f8d2bfb36c71dfeb4b0
8o5 <unitedmagic42@gmail.com>
2022-10-04 01:25:05 -0700

    product info scraping + logging done
    added product image + name scraping
    polished webhook logging

--- in_stock.py
+++ in_stock.py
@@ -5,6 +5,10 @@ import requests
 import toml
 from bs4 import BeautifulSoup
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+}
+
 config = toml.load("config.toml")
 
 match config:
@@ -18,15 +22,31 @@ match config:
         raise ValueError(f"invalid configuration: {config}")
 
 if config["settings"]["webhooks"] == True:
-    webhook = discord.SyncWebhook.from_url(config["webhook"])
+    webhook = discord.SyncWebhook.from_url(config["discord"]["webhook"])
 
+mention = config["discord"]["my_id"]
+
+def scrapeData():
+    data = requests.get(
+            url=config["url"]["url"],
+            headers=headers,
+            timeout=5,
+        )
+    z = BeautifulSoup(data.content, "html.parser")
+    img_location = z.findAll('img', class_="product-featured-media")
+    title_location = z.findAll('h1', class_="product-single__title")
+    product_image = f"https:{img_location[1]['src']}" # type: ignore
+    product_title = title_location[0].text
+    return (product_image, product_title)
+
+scrape = scrapeData()
 
 def notify():
     
     if config["settings"]["webhooks"] == True:
 
         embed = discord.Embed(
-            title=f"🎉 ITEM INSTOCK", color=0xCFC00, timestamp=discord.utils.utcnow()
+            title=f"🎉 ITEM INSTOCK", color=0xCFC00, timestamp=discord.utils.utcnow(), description=scrape[1]
         )
 
         embed.set_author(
@@ -38,7 +58,7 @@ def notify():
         embed.set_image(url="")  # set image
 
         embed.set_thumbnail(
-            url="https://cdn.shopify.com/s/files/1/0271/6018/2883/products/WHITE_SILVERSMUSH_1024x1024@2x.png?v=1662044356"
+            url=scrape[0]
         )  # set thumbnail
 
         embed.set_footer(
@@ -46,9 +66,9 @@ def notify():
             icon_url="https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png",
         )  # set footer
 
-        embed.add_field(name="URL", value=f"{config['url']}s", inline=False)
+        embed.add_field(name="URL", value=config['url']['url'], inline=False)
 
-        webhook.send(content=f"<@{config['my_id']}>", embed=embed)
+        webhook.send(content=f"<@{config['discord']['my_id']}>", embed=embed)
 
 
 def check(url, headers):
@@ -109,52 +129,45 @@ def check(url, headers):
             print(f"[{time.ctime()}] -- OOS")
 
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-}
+def initialize():
+    
+    print(f"SCANNING {config['url']['url']}")
+    
+    if config["settings"]["webhooks"] == True:
+        product_image = scrape[0]
+        product_title = scrape[1]
+        embed = discord.Embed(
+            title=f"🔎 STARTED SCANNING", color=0x161955, timestamp=discord.utils.utcnow(), description=product_title
+        )
 
-print(f"SCANNING {config['url']}")
+        embed.set_author(
+            name="Polyphia",
+            url="https://www.polyphia.com/",
+            icon_url="https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615",
+        )  # set author
 
-data = requests.get(
-        url=config["url"]["url"],
-        headers=headers,
-        timeout=5,
-    )
-z = BeautifulSoup(data.content, "html.parser")
-image_container = z.find('div', class_="product-single__media js-zoom-enabled")
-images = z.findAll('img')
-output = images[0]
-print(output)
+        embed.set_image(url="")  # set image
 
-if config["settings"]["webhooks"] == True:
-    embed = discord.Embed(
-        title=f"🔎 STARTED SCANNING", color=0x161955, timestamp=discord.utils.utcnow()
-    )
+        embed.set_thumbnail(
+            url=product_image
+        )  # set thumbnail
 
-    embed.set_author(
-        name=f"Polyphia",
-        url="https://www.polyphia.com/",
-        icon_url="https://cdn.shopify.com/s/files/1/0271/6018/2883/files/POLYPHIA_OW-3_180x.png?v=1657731615",
-    )  # set author
+        embed.set_footer(
+            text="nic#0002",
+            icon_url="https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png",
+        )  # set footer
 
-    embed.set_image(url="")  # set image
+        embed.add_field(name="URL", value=config["url"]["url"], inline=False)
 
-    embed.set_thumbnail(
-        url="https://cdn.shopify.com/s/files/1/0271/6018/2883/products/WHITE_SILVERSMUSH_1024x1024@2x.png?v=1662044356"
-    )  # set thumbnail
+        webhook.send(
+            content=f"<@{mention}>", 
+            embed=embed)
 
-    embed.set_footer(
-        text="nic#0002",
-        icon_url="https://cdn.discordapp.com/avatars/249547320306171907/d0f228743a5d8164043d75834abb755c.png",
-    )  # set footer
+    while True:
+        check(
+            url=config["url"]["url"],
+            headers=headers,
+        )
+        time.sleep(60)
 
-    embed.add_field(name="URL", value=f"{config['url']}s", inline=False)
-
-    webhook.send(content=f"<@{config['my_id']}>", embed=embed)
-
-while True:
-    check(
-        url=config["url"]["url"],
-        headers=headers,
-    )
-    time.sleep(60)
+initialize()
